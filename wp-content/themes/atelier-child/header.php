e6b1ec38106472d50e9317ff5a3b243152168665
arobbins <arobbins@simpleblend.net>
2015-10-10 14:34:21 -0500

    Added Favicons

--- wp-content/themes/atelier-child/header.php
+++ wp-content/themes/atelier-child/header.php
@@ -46,6 +46,24 @@
 
     <?php wp_head(); ?>
 
+    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/manifest.json">
+    <meta name="msapplication-TileColor" content="#4e1b17">
+    <meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri() ?>/assets/imgs/ms-icon-144x144.png">
+    <meta name="theme-color" content="#4e1b17">
+
   <!--// CLOSE HEAD //-->
   </head>
 
